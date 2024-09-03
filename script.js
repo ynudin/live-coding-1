cd8fae1ad7a3bf88ed9b85789221fc9ac8ed5099
Yana Udin <yn.udin1259@gmail.com>
2024-09-03 20:41:55 +0700

    add function

---
+++ script.js
@@ -0,0 +1,23 @@
+// // input        // result
+// cari 3 : 
+// [8 ,3 ,9 ,2 ,5] // 3
+
+// cari 1 : 
+// [8 ,3 ,9 ,2 ,5] // false
+
+// cari "h" : 
+// [8 ,3 ,9 ,2 ,5] // inputan harus number
+
+let arr = [8 ,3 ,9 ,2 ,5]
+let i = 3
+for (let i = 3; i != 3; i++) {
+  if (i = 3) {
+    3
+  } else if (i = ) {
+    false
+  } else {
+    
+  }
+
+
+}
